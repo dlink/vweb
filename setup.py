ece1a749995a4bd65d4028b16282155072e6fbac
David Link <dvlink@gmail.com>
2016-01-19 14:02:44 -0500

    updating Release notes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
     requirements = map(string.strip, open('requirements.txt').readlines())
 
 setup(name='vweb',
-      version='1.2.1',
+      version='1.2.2',
       description='Simple Python Website Frame work',
       long_description=readme,
       url='https://github.com/dlink/vweb',
